93d3bcee6446353531ecd23387ad5b60f11415e4
AntunesD <antunesd90@gmail.com>
2025-04-07 14:18:54 +0200

    lcov

--- front/karma.conf.js
+++ front/karma.conf.js
@@ -27,11 +27,7 @@ module.exports = function (config) {
     coverageReporter: {
       dir: require("path").join(__dirname, "./coverage/bobapp"),
       subdir: ".",
-      reporters: [
-        { type: "html" },
-        { type: "text-summary" },
-        { type: "cobertura", file: "cobertura-coverage.xml" },
-      ],
+      reporters: [{ type: "html" }, { type: "text-summary" }],
     },
     reporters: ["progress", "kjhtml"],
     port: 9876,
